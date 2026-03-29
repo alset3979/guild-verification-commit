9991fc2344c120943fff57d75eb4b3e3db573d5f
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:36:29 +0700

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:34:30.356Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-29T07:36:26.034Z
+Commit: Fix memory leak in event handlers
