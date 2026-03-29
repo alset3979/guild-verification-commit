07bd3f52437dddf32abdc77845748358aa85e3c1
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:37:11 +0700

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:37:03.307Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-29T07:37:07.537Z
+Commit: Fix memory leak in event handlers
