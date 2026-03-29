7e65eca1411a26d9e3015a5cf2a2b87f22d8adc1
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:37:33 +0700

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:37:19.363Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-29T07:37:29.709Z
+Commit: Fix CSS styling bug on mobile devices
