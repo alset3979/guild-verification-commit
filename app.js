b5aee7e76a1af32aec385710591b1ae78c8df8f3
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:37:29 +0700

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:37:23.267Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-29T07:37:25.371Z
+Commit: Resolve null pointer exception in user service
