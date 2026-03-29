3d68da3f575b9c6b22a65aabf5d1e9129973feed
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:34:05 +0700

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:33:48.964Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-29T07:34:01.476Z
+Commit: Resolve null pointer exception in user service
