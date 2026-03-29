be954c945bc9b921310739bbe1a533cffdb00cd1
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:33:53 +0700

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:33:45.340Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-29T07:33:48.964Z
+Commit: Fix memory leak in event handlers
