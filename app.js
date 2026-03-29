76605d3f12d1b730dd1fabc4ea1ab9ae403a235f
alset3979 <comnieunhasan@gmail.com>
2026-03-29 14:37:39 +0700

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-29T07:37:25.371Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-29T07:37:35.466Z
+Commit: Fix memory leak in event handlers
